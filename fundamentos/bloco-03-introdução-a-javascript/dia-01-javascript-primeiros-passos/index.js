935698c8c101bd1139669b4afb2a43091ba14fec
Guilherme Augusto <gulhermegui772001@gmail.com>
2023-01-23 19:27:11 -0300

    aula1 javascript

---
+++ fundamentos/bloco-03-introdução-a-javascript/dia-01-javascript-primeiros-passos/index.js
@@ -0,0 +1,36 @@
+//const a = 10
+//const b = 10
+//const adicao = a + b
+//const subtracao = a - b
+//const Multiplicação = (a * b)
+//const Divisao = (a / b)
+//const Modulo= (a % b)
+//console.log(subtracao)
+//console.log(adicao)
+//console.log(Multiplicação)
+//console.log(Divisao)
+//console.log(Modulo)
+/////////////////////////////////////////////////////////////////////
+let valor = 4
+let valor2 =15
+valor3 = 10
+if (valor > valor2 && valor > valor3){
+    console.log(valor)
+}
+
+else if (valor2 > valor && valor2> valor3)
+ {console.log(valor2) 
+}
+else {
+    console.log(valor3)
+}
+
+let soma = -4
+if (soma > 0){
+    console.log('positivo')
+}
+else if (soma === 0){
+    console.log(0)
+}else{
+    console.log('Negativo')
+}
